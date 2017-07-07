16e671f24190a79bc8428128ec3231cbff23a367
Odysseas <odysseasherodotou7@gmail.com>
2017-07-08 02:46:34 +0300

    Initial Commit

---
+++ Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Game
+//
+//  Created by Odysseas Herodotou on 08/07/2017.
+//  Copyright © 2017 example. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
